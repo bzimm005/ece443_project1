5d64b9fecf527ef646ef33111f369d00e3d14ebd
Brice Zimmerman <bricetoddzimmerman@gmail.com>
2023-10-19 15:31:06 -0400

    full adder waveform created and verified

--- ALU/src/TestBench/full_adder_TB.vhd
+++ ALU/src/TestBench/full_adder_TB.vhd
@@ -38,29 +38,61 @@ begin
 
 	-- Add your stimulus here ...
 	
-	-- Test Case 1: A=0, B=0, Cin=0
-  process
-  begin				
-    A <= '0';		-- plug in the values
-    B <= '0';
-    Cin <= '0';
-    wait for 10 ns; -- this is needed for simulation
-    assert (Sum = '0' and Cout = '0')  -- sum and cout should both be 0
-      report "Test Case 1 failed!" severity failure; -- report if sum and count aren't 0
-    wait;
-  end process;
-
-  -- Test Case 2: A=1, B=0, Cin=0
-  process
-  begin
-    A <= '1';		 -- plug in the values
-    B <= '0';
-    Cin <= '0';
-    wait for 10 ns;	 -- this is needed for simulation
-    assert (Sum = '1' and Cout = '0') -- sum should be 1 and cout 0
-      report "Test Case 2 failed!" severity failure; -- report if this is not so
-    wait;
-  end process;
+		  
+	  stimulus: process
+	  begin
+		  
+		-- Test Case 1: A=0, B=0, Cin=0  
+	    A <= '0';		-- plug in the values
+	    B <= '0';
+	    Cin <= '0';
+	    wait for 10 ns; -- this is needed for simulation
+	
+	    -- Test Case 2: A=0, B=0, Cin=1
+	    A <= '0';		 -- plug in the values
+	    B <= '0';
+	    Cin <= '1';
+	    wait for 10 ns;	 -- this is needed for simulation
+			
+		-- Test Case 3: A=0, B=1, Cin=0
+	    A <= '0';		 -- plug in the values
+	    B <= '1';
+	    Cin <= '0';
+	    wait for 10 ns;	 -- this is needed for simulation
+			
+		-- Test Case 4: A=0, B=1, Cin=1
+	    A <= '0';		 -- plug in the values
+	    B <= '1';
+	    Cin <= '1';
+	    wait for 10 ns;	 -- this is needed for simulation
+			
+		-- Test Case 5: A=1, B=0, Cin=0
+	    A <= '1';		 -- plug in the values
+	    B <= '0';
+	    Cin <= '0';
+	    wait for 10 ns;	 -- this is needed for simulation
+			
+		-- Test Case 6: A=1, B=0, Cin=1
+	    A <= '1';		 -- plug in the values
+	    B <= '0';
+	    Cin <= '1';
+	    wait for 10 ns;	 -- this is needed for simulation
+			
+		-- Test Case 7: A=1, B=1, Cin=0
+	    A <= '1';		 -- plug in the values
+	    B <= '1';
+	    Cin <= '0';
+	    wait for 10 ns;	 -- this is needed for simulation
+			
+		-- Test Case 3: A=1, B=1, Cin=1
+	    A <= '1';		 -- plug in the values
+	    B <= '1';
+	    Cin <= '1';
+	    wait for 10 ns;	 -- this is needed for simulation
+	    
+	    
+	      wait;		
+	  end process;
 
 end TB_ARCHITECTURE;
 
